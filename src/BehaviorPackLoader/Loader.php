e469f79c7b73cdd86c9df0eac1a94af525cd0e4e
DaisukeDaisuke <akblcmakblcm@gmail.com>
2021-01-01 15:02:54 +0900

    Loader: add ItemLoader concept (untested)

--- src/BehaviorPackLoader/Loader.php
+++ src/BehaviorPackLoader/Loader.php
@@ -3,11 +3,12 @@
 namespace BehaviorPackLoader;
 
 use BehaviorPackLoader\handler\BehaviorPacksHandler;
+use BehaviorPackLoader\handler\ItemLoader;
 use pocketmine\plugin\PluginBase;
 
 class Loader extends PluginBase{
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents(new BehaviorPacksHandler(), $this);
 		//$this->getServer()->getPluginManager()->registerEvents(new BehaviorPacksHandler($this->getDataFolder(),$this->getLogger(),false), $this);
+		ItemLoader::init($this->getDataFolder());
 	}
-}
