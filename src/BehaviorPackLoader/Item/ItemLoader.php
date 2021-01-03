c8c02db1eb7b1b39b2ad29761cca667b331784bb
DaisukeDaisuke <akblcmakblcm@gmail.com>
2021-01-03 09:03:10 +0900

    ItemHandler: Fix bug

--- src/BehaviorPackLoader/Item/ItemLoader.php
+++ src/BehaviorPackLoader/Item/ItemLoader.php
@@ -1,5 +1,8 @@
 <?php
 
+declare(strict_types=1);
+
+
 namespace BehaviorPackLoader\Item;
 
 use pocketmine\item\Item;
@@ -7,16 +10,19 @@ use pocketmine\item\ItemFactory;
 use pocketmine\item\ItemIdentifier;
 use pocketmine\network\mcpe\convert\ItemTranslator;
 use pocketmine\network\mcpe\convert\ItemTypeDictionary;
+use pocketmine\network\mcpe\protocol\types\ItemTypeEntry;
 use pocketmine\utils\Config;
 
 class ItemLoader{
+	public static $item_id_map;
+
 	public function __construct(){
 		//none
 	}
 
 	public static function init(String $confdir){
-		$item_id_map = (new Config($confdir."add_item_id_map.json", Config::JSON))->getAll();
-		self::register($item_id_map);
+		self::$item_id_map = (new Config($confdir."add_item_id_map.json", Config::JSON))->getAll();
+		self::register(self::$item_id_map);
 		self::registerItems();
 	}
 
@@ -26,14 +32,17 @@ class ItemLoader{
 	}
 
 	public static function register(array $item_id_map){
-		$stringToIntMap = [];
-		$intToStringIdMap = [];
+		//$stringToIntMap = [];
+		//$intToStringIdMap = [];
 
 		$simpleCoreToNetMapping = [];
 		$simpleNetToCoreMapping = [];
 
-		$runtimeId = self::bindTo(function(){
-			return max($this->stringToIntMap) + 1;
+		//$itemTypes = [];
+
+
+		[$runtimeId, $stringToIntMap, $intToStringIdMap, $itemTypes] = self::bindTo(function(){
+			return [max($this->stringToIntMap) + 4500, $this->stringToIntMap, $this->intToStringIdMap, $this->itemTypes];
 		}, ItemTypeDictionary::getInstance());
 
 		foreach($item_id_map as $string_id => $id){
@@ -42,21 +51,37 @@ class ItemLoader{
 
 			$simpleCoreToNetMapping[$id] = $runtimeId;
 			$simpleNetToCoreMapping[$runtimeId] = $id;
+
+			//var_dump("String_id: ".$string_id."\nruntimeId: ".$runtimeId."\nid: ".$id);
+	
+			$itemTypes[] = new ItemTypeEntry($string_id, $runtimeId, false);//true
+			//var_dump($itemTypes);
+			var_dump($string_id, $runtimeId, $id);
 			++$runtimeId;
 		}
-
-		self::bindTo(function() use ($stringToIntMap, $intToStringIdMap){
-			$this->stringToIntMap += $stringToIntMap;
-			$this->intToStringIdMap += $intToStringIdMap;
-		},ItemTypeDictionary::getInstance());
 
 		self::bindTo(function() use ($simpleCoreToNetMapping, $simpleNetToCoreMapping){
 			$this->simpleCoreToNetMapping += $simpleCoreToNetMapping;
 			$this->simpleNetToCoreMapping += $simpleNetToCoreMapping;
 		},ItemTranslator::getInstance());
+
+		self::bindTo(function() use ($itemTypes, $stringToIntMap, $intToStringIdMap){
+			$this->stringToIntMap += $stringToIntMap;
+			$this->intToStringIdMap += $intToStringIdMap;
+			$this->itemTypes += $itemTypes;
+			var_dump($this->itemTypes[0]);
+		},ItemTypeDictionary::getInstance());
 	}
+
 
 	public static function bindTo(\Closure $closure, $class){
 		return $closure->bindTo($class, get_class($class))();
+	}
+
+	public static function getItem_map(): ?array{
+		if(!isset(self::$item_id_map)){
+			return null;
+		}
+		return self::$item_id_map;
 	}
 }
